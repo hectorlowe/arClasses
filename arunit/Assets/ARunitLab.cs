a88610df8fcbc3ffb97b2b529cf6de4c9b0e3cd5
hectorlowe <hector.lowe@gmail.com>
2025-01-31 14:52:07 +0000

    Lab Session 3 partial
    couldn't really finish lab sheet 2, but used chatgpt for help. Didn't log in, so probably will lose the help. Oh no wait - I can't print the answers to Pdf and put them in the project folder. Oh no wait, I tried that and it didn't work.  Oh Well! Ask again next time!

--- arunit/Assets/ARunitLab.cs
+++ arunit/Assets/ARunitLab.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
@@ -8,6 +10,8 @@ public class ARunitLab : MonoBehaviour
 
 
 {
+    int plane_count = 0;
+    int point_count = 0;
     public TMP_Text text;
     [SerializeField] private TMP_Text _stateText;
     [SerializeField] private ARPlaneManager _arPlaneManager;
@@ -18,24 +22,71 @@ public class ARunitLab : MonoBehaviour
 
     private void OnARSessionStateChanged(ARSessionStateChangedEventArgs args)
     {
-        // Do something with args ...
-        // e.g. _stateText . text = args .[ XXX]
-        // Hint : you may want to look at what
-
+        // You could log or process session state changes here if needed
+        Debug.Log("AR session state changed: " + args.state);
     }
 
     private void Start()
     {
+        // Subscribe to AR session state changes
         ARSession.stateChanged += OnARSessionStateChanged;
+
+        // Subscribe to the planes changed event
+        _arPlaneManager.planesChanged += OnPlanesChanged;
 
     }
     private void OnPlanesChanged(ARPlanesChangedEventArgs args)
     {
-        args.ToString();
+        // This function will be called whenever the detected planes change (added, updated, or removed)
+
+        // Handle added planes
+        foreach (var plane in args.added)
+        {
+            Debug.Log("Plane added: " + plane.trackableId);
+        }
+
+        // Handle updated planes
+        foreach (var plane in args.updated)
+        {
+            Debug.Log("Plane updated: " + plane.trackableId);
+        }
+
+        // Handle removed planes
+        foreach (var plane in args.removed)
+        {
+            Debug.Log("Plane removed: " + plane.trackableId);
+        }
+
+        // Update the plane count (you could display this in the UI as well)
+        plane_count = _arPlaneManager.trackables.count;
+
+        // Update the UI with the new plane count
+        _planeText.text = $"Planes: {plane_count}";
     }
 
     private void Update()
     {
-        _planeText.ToString();
+        plane_count = _arPlaneManager.trackables.count;
+        point_count = 0;
+
+        // Calculate point count from ARPointCloudManager
+        foreach (var trackable in _arPointCloudManager.trackables)
+        {
+            if (trackable.positions is NativeSlice<Vector3> non_null_positions)
+            {
+                point_count += non_null_positions.Length;
+            }
+        }
+
+        // Update the UI text
+        _planeText.text = $"Planes: {plane_count}";
+        text.text = $"Points: {point_count}";
+
+    }
+    private void OnDestroy()
+    {
+        // Unsubscribe from events to avoid memory leaks
+        ARSession.stateChanged -= OnARSessionStateChanged;
+        _arPlaneManager.planesChanged -= OnPlanesChanged;
     }
 }
